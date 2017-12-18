056950b63d47a469635ab50e9e949370610f505f
liulv <469549118@qq.com>
2017-12-18 16:29:55 +0800

    Initial commit

---
+++ OnlinePreview/src/com/cectsims/util/Test.java
@@ -0,0 +1,13 @@
+package com.cectsims.util;
+
+public class Test {
+  public static void main(String[] args) {
+	String targetEendPoint="https://localhost:8888/axis/HelloAXIS.jws";
+//	Service service=new SerialArray();
+	//Call call=(Call)service.createCall();
+//	call.setOperationName(new QName(targetEendPoint),"Hello"));
+//	call.setTargetEndpointAddress(new URL(targetEendPoint));
+//	String result=(String)call.invoke(new Object[]{"lwp"});
+//	System.out.println(result);
+   }
+}
